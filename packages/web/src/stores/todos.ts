7224c3304ead5e2f65108e5ab7a0662e1099c3bd
Thom Bruce <thom@thombruce.com>
2023-05-16 18:30:30 +0100

    add createdAt and updatedAt values to todos

--- packages/web/src/stores/todos.ts
+++ packages/web/src/stores/todos.ts
@@ -3,19 +3,23 @@ import { defineStore } from 'pinia'
 
 export const useTodosStore = defineStore('todos', () => {
   // State
-  const list = ref([] as { done: boolean, text: string }[])
+  const list = ref([] as { done: boolean, text: string, createdAt: EpochTimeStamp, updatedAt: EpochTimeStamp }[])
 
   // Getters
   // const first = computed(() => list.value[0])
 
   // Actions
   function addTodo(text: string) {
-    list.value.push({ done: false, text })
+    var currentTime = Date.now()
+    list.value.push({ done: false, text, createdAt: currentTime, updatedAt: currentTime })
+    console.log(list.value)
   }
 
   function toggleTodo(text: string) {
+    var currentTime = Date.now()
     var i = list.value.findIndex(item => item.text === text)
-    list.value[i] = { ...list.value[i], ...{ done: !list.value[i].done } }
+    list.value[i] = { ...list.value[i], ...{ done: !list.value[i].done, updatedAt: currentTime } }
+    console.log(list.value[i])
   }
 
   function deleteTodo(text: string) {
