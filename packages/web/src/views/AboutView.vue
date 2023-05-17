b59fddc8cfaa171b6afaceb7a3725667b4ef4c2a
Thom Bruce <thom@thombruce.com>
2023-05-17 01:32:02 +0100

    add basic about info

--- packages/web/src/views/AboutView.vue
+++ packages/web/src/views/AboutView.vue
@@ -1,15 +1,20 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<template lang="pug">
+main
+  h1.text-xl.font-bold About Toodles
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+  p.
+    Toodles is an open source todo list app licensed under the
+    <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" class="text-blue-600 visited:text-purple-600">GNU General Public License Version 3 (GPLv3)</a>.
+    It was made with these awesome (and free) resources:
+  
+  ul.list-disc.list-inside
+    li Vue
+    li Vite
+    li Pinia
+    li Lerna
+    li Tailwind CSS
+    li Font Awesome
+
+//- TODO: Import and show version string from package.json
+//- TODO: Import Markdown document if possible (e.g. BACKERS.md)
+</template>
