d345240c0c41efe2b8e7e545659373145592ebcc
Thom Bruce <thom@thombruce.com>
2023-05-18 04:20:53 +0100

    add partitionAdapter comments

--- packages/web/src/plugins/loki.ts
+++ packages/web/src/plugins/loki.ts
@@ -3,13 +3,16 @@ import { useTodosStore } from '../stores/todos'
 
 import loki from 'lokijs'
 import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter'
+// import { LokiPartitioningAdapter } from 'lokijs'
 
 let dbName = 'toodles.db', db: any
 
 var idbAdapter = new LokiIndexedAdapter('toodles')
+// var partitionAdapter = new LokiPartitioningAdapter(idbAdapter) // Add { paging: true } if collections anticipated over 50Mb
 
 db = new loki(dbName, {
-  adapter: idbAdapter,
+  adapter: idbAdapter, // Comment out if using partitionAdapter instead
+  // adapter: partitionAdapter,
   autosave: true,
   autoload: true,
   autoloadCallback: initStore
