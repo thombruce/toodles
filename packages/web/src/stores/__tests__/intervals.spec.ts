6bbbf3f5641792b74fc3f85c48bc983e3d164260
Thom Bruce <thom@thombruce.com>
2023-05-18 23:07:52 +0100

    add interval basic actions

---
+++ packages/web/src/stores/__tests__/intervals.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+import { v4 as uuidv4 } from 'uuid'
+
+import { useIntervalsStore } from '../intervals'
+import type { UUID } from 'crypto'
+
+describe('Intervals Store', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+    useIntervalsStore().initStore()
+  })
+
+  it('starts intervals', () => {
+    const todos = useIntervalsStore()
+    let count = todos.list.data.length
+    todos.startInterval(uuidv4() as UUID)
+    expect(todos.list.data.length).toBe(count + 1)
+  })
+
+  it('stops intervals', () => {
+    const todos = useIntervalsStore()
+    todos.startInterval(uuidv4() as UUID)
+    var id = todos.list.data[0].id
+    expect(todos.list.data[0].startedAt).toBeLessThanOrEqual(Date.now())
+    todos.stopInterval(id)
+    expect(todos.list.data[0].duration).toBeGreaterThan(0)
+  })
+
+  it('removes intervals', () => {
+    const todos = useIntervalsStore()
+    todos.startInterval(uuidv4() as UUID)
+    let count = todos.list.data.length
+    var id = todos.list.data[0].id
+    todos.deleteInterval(id)
+    expect(todos.list.data.length).toBe(count - 1)
+  })
+})
--- packages/web/src/stores/intervals.ts
+++ packages/web/src/stores/intervals.ts
@@ -6,7 +6,14 @@ import { Collection } from 'lokijs'
 
 import db from '../plugins/loki'
 
-interface Interval {}
+interface Interval {
+  id: UUID,
+  todoId: UUID,
+  startedAt?: EpochTimeStamp,
+  duration?: number,
+  createdAt: EpochTimeStamp,
+  updatedAt: EpochTimeStamp
+}
 
 interface List extends Collection {
   insert(todo: Interval | Interval[]): Interval[] | undefined,
@@ -29,6 +36,23 @@ export const useIntervalsStore = defineStore('intervals', () => {
     }
   }
 
+  function startInterval(todoId: UUID) {
+    var currentTime = Date.now()
+    list.value.insert({ id: uuidv4() as UUID, todoId, startedAt: currentTime, createdAt: currentTime, updatedAt: currentTime })
+  }
+
+  function stopInterval(id: UUID) {
+    var interval = list.value.find({ id })[0] as Interval
+    if (!interval.startedAt || interval.duration) return
+    var currentTime = Date.now()
+    var duration = currentTime - interval.startedAt
+    list.value.update({ ...interval, ...{ duration, updatedAt: currentTime } })
+  }
+
+  function deleteInterval(id: UUID) {
+    list.value.chain().find({ id }).remove()
+  }
+
   // Export
-  return { list, initStore }
+  return { list, initStore, startInterval, stopInterval, deleteInterval }
 })
