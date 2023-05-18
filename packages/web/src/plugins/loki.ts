c5c1e0b1ff2e774ebb9d3b3ee569e45b714ef9e2
Thom Bruce <thom@thombruce.com>
2023-05-18 04:25:30 +0100

    add fsStructuredAdapter comments

--- packages/web/src/plugins/loki.ts
+++ packages/web/src/plugins/loki.ts
@@ -4,15 +4,18 @@ import { useTodosStore } from '../stores/todos'
 import loki from 'lokijs'
 import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter'
 // import { LokiPartitioningAdapter } from 'lokijs'
+// import LokiFsStructuredAdapter from 'lokijs/src/loki-fs-structured-adapter'
 
 let dbName = 'toodles.db', db: any
 
 var idbAdapter = new LokiIndexedAdapter('toodles')
 // var partitionAdapter = new LokiPartitioningAdapter(idbAdapter) // Add { paging: true } if collections anticipated over 50Mb
+// var fsStructuredAdapter = new LokiFsStructuredAdapter()
 
 db = new loki(dbName, {
   adapter: idbAdapter, // Comment out if using partitionAdapter instead
   // adapter: partitionAdapter,
+  // adapter: fsStructuredAdapter,
   autosave: true,
   autoload: true,
   autoloadCallback: initStore
