59455f2ef18e7781f82e8a4c39c26f24b05a6325
Thom Bruce <thom@thombruce.com>
2023-05-18 04:40:05 +0100

    add nativeScriptAdapter comments

--- packages/web/src/plugins/loki.ts
+++ packages/web/src/plugins/loki.ts
@@ -5,17 +5,20 @@ import loki from 'lokijs'
 import LokiIndexedAdapter from 'lokijs/src/loki-indexed-adapter'
 // import { LokiPartitioningAdapter } from 'lokijs'
 // import LokiFsStructuredAdapter from 'lokijs/src/loki-fs-structured-adapter'
+// import LokiNativeScriptAdapter from 'lokijs/src/loki-nativescript-adapter'
 
 let dbName = 'toodles.db', db: any
 
 var idbAdapter = new LokiIndexedAdapter('toodles')
 // var partitionAdapter = new LokiPartitioningAdapter(idbAdapter) // Add { paging: true } if collections anticipated over 50Mb
 // var fsStructuredAdapter = new LokiFsStructuredAdapter()
+// var nativeScriptAdapter = new LokiNativescriptAdapter()
 
 db = new loki(dbName, {
   adapter: idbAdapter, // Comment out if using partitionAdapter instead
   // adapter: partitionAdapter,
   // adapter: fsStructuredAdapter,
+  // adapter: nativeScriptAdapter,
   autosave: true,
   autoload: true,
   autoloadCallback: initStore
