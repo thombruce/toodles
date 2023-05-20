b0863f3a8943f97c094841442093fa5b2d7e48d1
Thom Bruce <thom@thombruce.com>
2023-05-20 10:12:45 +0100

    add IntervalTimer and TodoItem component tests

---
+++ packages/web/src/components/__tests__/TodoItem.spec.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
+
+import { mount } from '@vue/test-utils'
+
+import { useTodosStore } from '@/stores/todos'
+import { useIntervalsStore } from '@/stores/intervals'
+import TodoItem from '../TodoItem.vue'
+
+vi.mock('vue-router', () => ({
+  resolve: vi.fn(),
+  RouterLink: vi.fn()
+}))
+
+const wrapper = mount(TodoItem, {
+  props: {
+    todo: {
+      id: "someId",
+      text: "Get milk!"
+    }
+  }
+})
+
+describe('TodoItem', () => {
+  beforeEach(() => {
+    // TODO: We should probably createTestingPinia and setup mocks of internal actions
+    setActivePinia(createPinia())
+    useIntervalsStore().initStore()
+    useTodosStore().initStore()
+  })
+
+  it('renders properly', () => {
+    expect(wrapper.html()).toContain('<div')
+  })
+})
