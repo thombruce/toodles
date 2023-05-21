271fbb4ae28532880e8d84c13d1d81689f66c957
Thom Bruce <thom@thombruce.com>
2023-05-21 12:55:05 +0100

    add basic interval form

--- packages/web/src/components/IntervalList.vue
+++ packages/web/src/components/IntervalList.vue
@@ -13,11 +13,22 @@ const props = defineProps({
   }
 })
 
+const interval = ref({ dateOf: '', duration: null })
+
 const store = useIntervalsStore()
 
 const { forTodo } = storeToRefs(store)
 
 const { deleteInterval } = store
+
+function addIntervalAndClear(item: { dateOf: string, duration: number}) {
+  if (item.dateOf.length === 0) {
+    return
+  }
+
+  store.addInterval(props.todoId, item.dateOf, item.duration)
+  interval.value = { dateOf: '', duration: null }
+}
 </script>
 
 <template lang="pug">
@@ -27,4 +38,12 @@ ul.mb-4
       span {{ timepiece(interval.duration) }}
       span {{ new Date(interval.dateOf) }}
       button.text-red-600(@click="deleteInterval(interval.id)") Delete
+
+//- TODO: Add labels
+form.space-x-4(@submit.prevent="addIntervalAndClear(interval)")
+  //- TODO: Allow time value
+  input(v-model="interval.dateOf" type="date" class="border rounded py-2 px-3 text-gray-700" ref="newIntervalDateOf")
+  //- TODO: Input as string
+  input(v-model="interval.duration" type="number" class="border rounded py-2 px-3 text-gray-700" ref="newIntervalDuration")
+  button(class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") Add
 </template>
