49ce72e8fc1262523ebf52141e09fcd7fb41dd07
Thom Bruce <thom@thombruce.com>
2023-05-23 09:09:14 +0100

    add empty tests for main models

---
+++ packages/web/src/models/__tests__/Comment.spec.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest'
+import { Comment } from '../Comment'
+
+describe.todo('Comment', () => {
+})
---
+++ packages/web/src/models/__tests__/Interval.spec.ts
@@ -0,0 +1,5 @@
+import { describe, it, expect } from 'vitest'
+import { Interval } from '../Interval'
+
+describe.todo('Interval', () => {
+})
