81e77207bdea6230268e5006558884c9e71a4b38
Thom Bruce <thom@thombruce.com>
2023-06-01 17:27:05 +0100

    move store usage to views rather than component parts

--- packages/web/src/components/TodoList.vue
+++ packages/web/src/components/TodoList.vue
@@ -1,35 +1,16 @@
 <script setup lang="ts">
-import { computed } from "vue"
-import { storeToRefs } from "pinia"
-
-import { useTodosStore } from "../stores/todos"
-
 import TodoItem from "./TodoItem.vue"
 
 const props = defineProps({
-  project: {
-    type: String,
-    required: false
-  },
-  context: {
-    type: String,
-    required: false
+  todos: {
+    type: Array,
+    required: true
   }
-})
-
-const store = useTodosStore()
-
-const { list, forProject, forContext } = storeToRefs(store)
-
-const todoList = computed(() => {
-  if (props.project) return forProject.value(props.project)
-  if (props.context) return forContext.value(props.context)
-  return list.value.data
 })
 </script>
 
 <template lang="pug">
 ul.mb-4
-  li(v-for="todo in todoList" :key="todo.id")
+  li(v-for="todo in todos" :key="todo.id")
     TodoItem(:todo="todo")
 </template>
