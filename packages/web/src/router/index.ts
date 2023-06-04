5ec404d4935590ee182b7bc551e842b5cf15a55a
Thom Bruce <thom@thombruce.com>
2023-06-04 01:14:54 +0100

    use router hash history mode

--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/todos/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
+  // history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
