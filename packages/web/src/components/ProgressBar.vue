a227c86a1a17050cda8ed604c22fdc923c9130b2
Thom Bruce <thom@thombruce.com>
2023-06-04 15:12:46 +0100

    add progress bar to todo lists

---
+++ packages/web/src/components/ProgressBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+  progress: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
+
+<template lang="pug">
+.w-full.bg-gray-200.rounded-full(class='h-2.5')
+  .bg-emerald-500.rounded-full(class='h-2.5 transition-[width] ease-in-out duration-500' :style="'width:'+progress+'%;'")
+</template>
