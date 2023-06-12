a3bf59ebf8845033104a6d7c478b14c345ab45ed
Thom Bruce <thom@thombruce.com>
2023-06-12 23:47:59 +0100

    remove search nav link

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -6,7 +6,6 @@ header.mb-4
   strong Toodles
   nav.space-x-4
     RouterLink(to="/") Home
-    RouterLink(to="/s") Search
     RouterLink(to="/about") About
 RouterView(:key="$route.fullPath")
 </template>
