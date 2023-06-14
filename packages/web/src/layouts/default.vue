8f6b4fe4b6e4280d9084d8f13c632035b99f5e7b
Thom Bruce <thom@thombruce.com>
2023-06-15 00:57:55 +0100

    add layouts

---
+++ packages/web/src/layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import MultiBar from "@/components/MultiBar.vue"
+</script>
+
+<template lang="pug">
+MultiBar.mb-4
+main
+  slot
+</template>
--- packages/web/src/router/index.ts
+++ packages/web/src/router/index.ts
@@ -41,7 +41,10 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about.vue')
+      component: () => import('../views/about.vue'),
+      meta: {
+        layout: 'info'
+      }
     }
   ]
 })
