d747fb745836d569670d22c61bcc9d0eda7558ab
Thom Bruce <thom@thombruce.com>
2023-06-15 01:04:23 +0100

    clear MultiBar on escape keypress

--- packages/web/src/components/MultiBar.vue
+++ packages/web/src/components/MultiBar.vue
@@ -27,10 +27,15 @@ function addTodoAndClear(item: string) {
   text.value = ""
   search(text.value)
 }
+
+function clear() {
+  text.value = ""
+  search(text.value)
+}
 </script>
 
 <template lang="pug">
 form.flex.w-full.space-x-4(@submit.prevent="addTodoAndClear(text)")
-  input.grow(v-model="text" type="text" class="border rounded py-2 px-3 text-gray-700" @input="search(text)" @keydown.ctrl.enter="globalSearch(text)")
+  input.grow(v-model="text" type="text" class="border rounded py-2 px-3 text-gray-700" @input="search(text)" @keydown.ctrl.enter="globalSearch(text)" @keydown.esc="clear();$event.target.blur()")
   button(class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded") Add
 </template>
