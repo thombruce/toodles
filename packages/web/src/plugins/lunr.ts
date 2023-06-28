bfc160f105a6d89850d4556040855247fac80b24
Thom Bruce <thom@thombruce.com>
2023-06-28 20:04:24 +0100

    separate tokens on any non-word characters

--- packages/web/src/plugins/lunr.ts
+++ packages/web/src/plugins/lunr.ts
@@ -4,7 +4,7 @@ import { uniq as _uniq } from 'lodash'
 
 const index = lunr(() => {}) as any
 
-lunr.tokenizer.separator = /[+@:\s\-]+/
+lunr.tokenizer.separator = /\W+/
 
 const getTokenStream = (text: string) =>
   index.pipeline.run(
