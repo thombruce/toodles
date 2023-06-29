5273a6dd71d2a751119e609e6f7e6808446c27ab
Thom Bruce <thom@thombruce.com>
2023-06-29 01:46:36 +0100

    set blank base by default and configure with environment variable

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/toodles/' : '/'
+  base: process.env.TOODLES_BASE || '/'
 })
