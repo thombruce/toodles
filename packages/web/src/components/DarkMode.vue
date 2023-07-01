b98e12586b2057a93a9a278f1024e35cebcb50e8
Thom Bruce <thom@thombruce.com>
2023-07-01 18:18:31 +0100

    store locale in localStorage

--- packages/web/src/components/DarkMode.vue
+++ packages/web/src/components/DarkMode.vue
@@ -3,11 +3,11 @@ import { ref } from 'vue'
 
 const theme = ref(localStorage.theme || 'system')
 
-const toggleDarkMode = (theme: string) => {
-  if (theme === 'system') {
+const toggleDarkMode = (event: any) => {
+  if (event.target.value === 'system') {
     localStorage.removeItem('theme')
   } else {
-    localStorage.theme = theme
+    localStorage.theme = event.target.value
   }
 
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -21,12 +21,12 @@ const toggleDarkMode = (theme: string) => {
 <template lang="pug">
 .space-x-4
   .inline.space-x-1
-    input(type="radio" id="light" name="darkmode" value="light" v-model="theme" @click="toggleDarkMode('light')")
+    input(type="radio" id="light" name="darkmode" value="light" v-model="theme" @change="toggleDarkMode($event)")
     label(for="light") {{ $t("conditions.light") }}
   .inline.space-x-1
-    input(type="radio" id="dark" name="darkmode" value="dark" v-model="theme" @click="toggleDarkMode('dark')")
+    input(type="radio" id="dark" name="darkmode" value="dark" v-model="theme" @change="toggleDarkMode($event)")
     label(for="dark") {{ $t("conditions.dark") }}
   .inline.space-x-1
-    input(type="radio" id="system" name="darkmode" value="system" v-model="theme" @click="toggleDarkMode('system')")
+    input(type="radio" id="system" name="darkmode" value="system" v-model="theme" @change="toggleDarkMode($event)")
     label(for="system") {{ $t("conditions.system") }}
 </template>
