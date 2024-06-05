7f79b83fe34d783c505da5615e2de717aa177996
Thom Bruce <thom@thombruce.com>
2024-06-05 22:19:14 +0100

    add unique colors for prices

--- packages/vscode/src/utils/StyleConstants.ts
+++ packages/vscode/src/utils/StyleConstants.ts
@@ -3,14 +3,19 @@
 export default class StyleConstants {
   public static readonly PROJECT_DARK = 'rgb(186, 234, 244)';
   public static readonly PROJECT_LIGHT = 'rgb(21, 141, 168)';
+
   public static readonly CONTEXT_DARK = 'rgb(190, 244, 186)';
   public static readonly CONTEXT_LIGHT = 'rgb(91, 124, 88)';
+
   public static readonly HASHTAG_DARK = 'rgb(244, 124, 42)';
   public static readonly HASHTAG_LIGHT = 'rgb(124, 88, 13)';
-  public static readonly PRICE_DARK = 'rgb(244, 124, 42)';
-  public static readonly PRICE_LIGHT = 'rgb(124, 88, 13)';
+
+  public static readonly PRICE_DARK = 'rgb(100, 180, 92)';
+  public static readonly PRICE_LIGHT = 'rgb(64, 124, 60)';
+
   public static readonly DATE_DARK = 'rgb(244, 244, 186)';
   public static readonly DATE_LIGHT = 'rgb(135, 135, 29)';
+
   public static readonly PRIORITY_DARK = 'rgb(209, 186, 244)';
   public static readonly PRIORITY_LIGHT = 'rgb(117, 43, 229)';
 
