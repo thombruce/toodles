3461a64e35b81e68da2ead823c8478aa20e2d644
Thom Bruce <thom@thombruce.com>
2024-06-05 18:44:19 +0100

    enable decorator for live editing

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -2,29 +2,136 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import ToodlesDecorator from './decorators/ToodlesDecorator';
+import * as path from 'path';
+
+import AppConstants from './utils/AppConstants';
+import StyleConstants from './utils/StyleConstants';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  let toodlesDecorator = new ToodlesDecorator();
+	let timeout: NodeJS.Timeout | undefined = undefined;
 
-  // By Default Decorate the document
-  toodlesDecorator.decorateDocument();
+	// Decoration Types
+	const dateDecorationType = vscode.window.createTextEditorDecorationType({
+		light: {
+            color: StyleConstants.DATE_LIGHT
+        },
+        dark: {
+            color: StyleConstants.DATE_DARK
+        }
+    });
+    const projectDecorationType = vscode.window.createTextEditorDecorationType({
+		light: {
+            color: StyleConstants.PROJECT_LIGHT
+        },
+        dark: {
+            color: StyleConstants.PROJECT_DARK
+        }
+    });
+    const priorityDecorationType = vscode.window.createTextEditorDecorationType({
+		light: {
+            color: StyleConstants.PRIORITY_LIGHT
+        },
+        dark: {
+            color: StyleConstants.PRIORITY_DARK
+        }
+    });
+    // const overdueDecorationType = vscode.window.createTextEditorDecorationType({});
+    const contextDecorationType = vscode.window.createTextEditorDecorationType({
+		light: {
+            color: StyleConstants.CONTEXT_LIGHT
+        },
+        dark: {
+            color: StyleConstants.CONTEXT_DARK
+        }
+    });
+    const completedDecorationType = vscode.window.createTextEditorDecorationType({
+        textDecoration: StyleConstants.COMPLETED_CSS
+    });
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+
+		let fileName = path.basename(activeEditor.document.fileName);
+		if (!(AppConstants.ACCEPTED_FILENAMES.lastIndexOf(fileName) >= 0 || fileName.match(AppConstants.FILENAMES_REGEX))) {
+			return;
+		}
+
+		const dates: vscode.DecorationOptions[] = [];
+		const projects: vscode.DecorationOptions[] = [];
+		const priorities: vscode.DecorationOptions[] = [];
+		// const overdue: vscode.DecorationOptions[] = [];
+		const completed: vscode.DecorationOptions[] = [];
+		const contexts: vscode.DecorationOptions[] = [];
+
+        // Iterate over each line and parse accordingly
+        let totalLines = activeEditor.document.lineCount;
+        for (var i = 0; i <= totalLines - 1; i++) {
+            let lineObject = activeEditor.document.lineAt(i);
+
+            parseRegex(AppConstants.DATE_REGEX, dates, lineObject);
+            parseRegex(AppConstants.PROJECT_REGEX, projects, lineObject);
+            parseRegex(AppConstants.CONTEXT_REGEX, contexts, lineObject);
+            parseRegex(AppConstants.PRIORITY_REGEX, priorities, lineObject);
+
+            if (lineObject.text.startsWith("x ") || lineObject.text.startsWith("X ")) {
+                let decoration = { range: lineObject.range};
+                completed.push(decoration);
+            }
+        }
+
+		activeEditor.setDecorations(dateDecorationType, dates);
+        activeEditor.setDecorations(projectDecorationType, projects);
+        activeEditor.setDecorations(contextDecorationType, contexts);
+        activeEditor.setDecorations(completedDecorationType, completed);
+		// activeEditor.setDecorations(overdueDecorationType, overdue);
+        activeEditor.setDecorations(priorityDecorationType, priorities);
+	}
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations(true);
+		}
+	}, null, context.subscriptions);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-
-// TODO: This isn't working, and I know why!
-// Compare to: https://github.com/microsoft/vscode-extension-samples/blob/main/decorator-sample/src/extension.ts
-// Notice the use of onDidChange______ events
-//
-// The work I've been basing this on so far, I think, must use an older version of the VSCode API.
-// Makes sense; it was last touched 7 years ago.
-// I just need to make modifications such that my baseline is the linked example above...
-// ...and add my specific work on top of that.
-// This should fix the problem of...
-// - It currently only works when FIRST document is FIRST opened
-// - Fails for second document and beyond
+function parseRegex(iRegExp: RegExp, decorationOptions: vscode.DecorationOptions[], inputLine: vscode.TextLine) {
+	let match: RegExpExecArray;
+	while (match = iRegExp.exec(inputLine.text)) {
+		let beginPosition = new vscode.Position(inputLine.range.start.line, inputLine.firstNonWhitespaceCharacterIndex + match.index);
+		let endPosition = new vscode.Position(inputLine.range.start.line, inputLine.firstNonWhitespaceCharacterIndex + match.index + match[0].length);
+		let decoration = { range: new vscode.Range(beginPosition, endPosition) };
+		decorationOptions.push(decoration);
+	}
+}
