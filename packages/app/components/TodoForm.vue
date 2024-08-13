b17fe9241c4de565fead2c3692b60b3431938593
Thom Bruce <thom@thombruce.com>
2024-08-13 06:09:15 +0100

    add basic advanced Todo inputs

--- packages/app/components/TodoForm.vue
+++ packages/app/components/TodoForm.vue
@@ -6,7 +6,15 @@ const store = useTodosStore()
 // Store: Actions
 const { addTodo } = store
 
-const newTodo = ref({ priority: undefined, due: undefined, description: "" })
+const showAdvanced = ref(false)
+
+const newTodo = ref({
+  priority: undefined,
+  due: undefined,
+  description: "",
+  price: undefined,
+  multiplier: undefined
+})
 
 function addTodoAndClear() {
   if (newTodo.value.description.length === 0) {
@@ -18,27 +26,53 @@ function addTodoAndClear() {
 }
 
 function clear() {
-  newTodo.value = { priority: undefined, due: undefined, description: "" }
+  newTodo.value = {
+    priority: undefined,
+    due: undefined,
+    description: "",
+    price: undefined,
+    multiplier: undefined
+  }
   // search()
 }
 </script>
 
 <template lang="pug">
-TntForm.flex.w-full.space-x-4.mb-3(@submit="addTodoAndClear()")
-  TntSelect.flex-0.min-w-16(
-    v-model="newTodo.priority"
-    :options="['', 'A', 'B', 'C']"
-  )
+TntForm.mb-3(@submit="addTodoAndClear()")
+  .flex.w-full.space-x-4
+    TntInput.flex-1(
+      v-model="newTodo.description"
+      @keydown.esc="clear();$event.target.blur()"
+    )
 
-  TntInput.flex-0(
-    v-model="newTodo.due"
-    type="date"
-  )
+    TntButton.btn-none.text-primary(@click="showAdvanced = !showAdvanced") Advanced
 
-  TntInput.flex-1(
-    v-model="newTodo.description"
-    @keydown.esc="clear();$event.target.blur()"
-  )
+    TntSubmit Add
 
-  TntSubmit Add
+  .flex.w-full.space-x-4(v-show="showAdvanced")
+    TntSelect.flex-1(
+      v-model="newTodo.priority"
+      label="Priority"
+      :options="['', 'A', 'B', 'C']"
+    )
+
+    TntInput.flex-1(
+      v-model="newTodo.due"
+      label="Due"
+      type="date"
+    )
+
+    //- TODO: Better money inputs! This should validate as a number,
+    //-       but still result in a string with a supported currency
+    //-       symbol.
+    TntInput.flex-1(
+      v-model="newTodo.price"
+      label="Price"
+    )
+
+    TntInput.flex-1(
+      v-model="newTodo.multiplier"
+      label="Amount"
+      type="number"
+    )
 </template>
